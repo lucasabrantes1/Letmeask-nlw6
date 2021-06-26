fc3e7b45c765cc3c99f5d5cb3338bc55449d1340
Lucas Silva Dantas Abrantes <lucasabrantes002@gmail.com>
2021-06-25 22:23:02 -0300

    Estipulando autorização

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,75 @@
+import { useHistory } from 'react-router-dom'
+import { FormEvent, useState } from 'react';
 
-import Illustration from '../assets/images/illustration.svg'
-import Logo from '../assets/images/logo.svg'
-import GoogleIcon from '../assets/images/google-icon.svg'
-import '../styles/auth.scss'
-import { Button } from '../components/Button'
-import { useHistory } from 'react-router'
-// import { AuthContext } from '../context/AuthContext'
-// import { useContext} from 'react'
-import { useAuth } from '../hooks/useAuth'
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
 
+import { database } from '../services/firebase';
 
-export function Home(){
-    const history = useHistory();
-    const {user, signInWithGoogle } = useAuth()
-    
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
-    async function handleCreateRoom(){
+import '../styles/auth.scss';
 
-        if(!user){
-          await  signInWithGoogle()
-        }
+export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode] = useState('');
 
-        history.push('/rooms/news');        
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
     }
 
+    history.push('/rooms/new');
+  }
 
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={Illustration} alt="illustration" />
-                <strong>Crie salas de Q&A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo real</p>
-            </aside>
-            <main>
-                
-                <div className='main-content'>
-                    <img src={Logo} alt="Logo" />
-                    <button onClick={handleCreateRoom} className="create-room">
-                        <img src={GoogleIcon} alt="Google" />
-                        Crie sua sala com o google
-                    </button>
-                    <div className="separator">ou entre uma sala</div>
-                    <form>
-                        <input type="text" placeholder="Digite o código da sala" />
-                        <Button type="submit">
-                          Entrar na sala
-                       </Button>
-                    </form>
-                </div>
-            </main>
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input 
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
         </div>
-    )
- }
+      </main>
+    </div>
+  )
+}
